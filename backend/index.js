f3628927ca8bd2801249097f1588b26fd57f8f7e
mohammedhasan-prog <mohammed.hasan22b@iiitg.ac.in>
2025-01-14 17:24:07 +0530

    more back end update

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,62 @@
 const express=require('express')
-import { creatTodo,updateTodo } from './typs';
+const  { creatTodo,updateTodo } =require( './typs');
+const {todo}=require('./db')
+const cors=require('cors')
 
 const app=express();
 app.use(express.json())
+app.use(cors());
 
-app.post('/todo',(req,res)=>{
+app.post('/todo',async (req,res)=>{
     const {tital,description,isCompleted}=req.body;
 
+    const validation=creatTodo.safeParse({
+        tital,
+        description,
+         
+    })
+if (!validation.success){
+    res.status(404).json({
+        msg:"write correct input"
+    })
+
+   await  todo.create(({
+        tital,description,isCompleted
+    }))
+    res.json({
+        msg:"Todo create succesfully"
+    })
+}
 
    
 })
 
-app.get('/todos',(req,res)=>{
-
+app.get('/todos',async (req,res)=>{
+const todos=await todo.find();
+res.json({todos});
 })
 
-app.put('/complet',(req,res)=>{
+app.put('/completed',async (req,res)=>{
+    const _id=req.body.id;
 
+    const Updatevalidation=updateTodo.safeParse({
+        id:_id
+    })
+if (!Updatevalidation.success){
+    res.status(404).json({
+        msg:"write correct input"
+    })
+
+    await  todo.update({
+        _id:_id
+    },{
+        isCompleted:true
+    })
+
+    res.json({
+        msg:"mark as completed"
+    })
+}
 })
 app.listen(3000,()=>{
     console.log("connect to server")
